d7a10bf493233fcfe02cbc73b2596e844a8b8d1e
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-11 17:42:50 +0900

    Fix topic name

--- Assets/Scripts/Sensors/RGBCamera/RGBCameraPublisher.cs
+++ Assets/Scripts/Sensors/RGBCamera/RGBCameraPublisher.cs
@@ -12,15 +12,17 @@ namespace RobotSensors
         private RGBCameraSerializer _serializer;
 
         [SerializeField]
-        private string _topicName = "imu/raw_data";
+        private string _topicName = "image";
         [SerializeField]
-        private string _frame_id = "imu_link";
+        private string _frame_id = "camera";
 
         protected override void Init()
         {
             _ros.RegisterPublisher<CompressedImageMsg>(_topicName);
             _serializer = new RGBCameraSerializer();
             _serializer.Init(_frame_id);
+            
+            _topicName += "/compressed";
         }
 
         protected override void Publish(float time)
