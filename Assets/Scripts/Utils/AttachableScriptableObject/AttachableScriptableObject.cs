2603d73b67a0b71601fe4c3095e3acb75db35d75
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-11 12:23:49 +0900

    Add AttachableScriptableObject

---
+++ Assets/Scripts/Utils/AttachableScriptableObject/AttachableScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachableScriptableObject : ScriptableObject
+{
+    public virtual void Update()
+    {
+
+    }
+}
