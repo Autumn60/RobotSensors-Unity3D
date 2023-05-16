0ffc9629356c7be953b3c94a71246df36546bc33
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-05-16 16:53:04 +0900

    Create LiDAR sensor

--- Assets/Scripts/Sensors/LiDAR/LiDARSensor.cs
+++ Assets/Scripts/Sensors/LiDAR/LiDARSensor.cs
@@ -2,16 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Unity.Burst;
+using Unity.Collections;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+
 namespace RobotSensors
 {
     public class LiDARSensor : Sensor
     {
+        protected JobHandle _handle;
+
+        protected NativeArray<Vector3> _points;
+        public NativeArray<Vector3>.ReadOnly points { get => GetPoints(); }
+
         protected override void Init()
         {
         }
 
         protected override void UpdateSensor()
         {
+        }
+
+        protected virtual NativeArray<Vector3>.ReadOnly GetPoints()
+        {
+            _handle.Complete();
+            return _points.AsReadOnly();
         }
     }
 }
--- Assets/Scripts/Sensors/LiDAR/LiDARVisualizer.cs
+++ Assets/Scripts/Sensors/LiDAR/LiDARVisualizer.cs
@@ -7,8 +7,41 @@ namespace RobotSensors
     [RequireComponent(typeof(LiDARSensor))]
     public class LiDARVisualizer : Visualizer<LiDARSensor>
     {
+        [SerializeField]
+        private LiDARSensor _sensor;
+
+        [SerializeField]
+        private SphereSetting _point;
+
+        [SerializeField]
+        private int _maxPointNum = 1000;
+
+        private Transform _transform;
+
+        protected override void Update()
+        {
+            base._visualizeMode_edit = Visualizer<LiDARSensor>.VisualizeMode.NONE;
+        }
+
         protected override void Visualize()
         {
+            if (!_target) return;
+            if (!_transform) _transform = this.transform;
+
+            Gizmos.color = _point.color;
+            
+            for (int i = 0; i < (_maxPointNum < _target.points.Length ? _maxPointNum : _target.points.Length); i++)
+            {
+                int index = (_maxPointNum < _target.points.Length ? UnityEngine.Random.Range(0, _target.points.Length) : i);
+                Gizmos.DrawSphere(_transform.TransformPoint(_target.points[index]), _point.radius);
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (_target) return;
+            if (!_sensor) return;
+            _target = _sensor as LiDARSensor;
         }
     }
 }
